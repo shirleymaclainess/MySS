611c8426d160a7523ffcae49d9d08ee5256f318e
shirleymaclainess <shirleymaclainess@gmail.com>
2026-05-11 20:27:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'myss-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalação: Salva os arquivos essenciais no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação: Limpa caches antigos se houver
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Busca: Serve os arquivos do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
